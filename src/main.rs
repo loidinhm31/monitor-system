02b2842201a5d0c7d12dbf35abfd96b9606b5235
loidinhm31 <loidinh.git@gmail.com>
2024-05-18 22:01:11 +0700

    Convert to use Server-Sent Events

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use actix::{Actor, StreamHandler, AsyncContext};
-use actix_web::{web, App, HttpServer, HttpRequest, Error, HttpResponse};
-use actix_web::web::Payload;
+use std::sync::{Arc, Mutex};
+
 use actix_cors::Cors;
-use actix_web_actors::ws;
+use actix_web::{App, Error, HttpRequest, HttpResponse, HttpServer, web};
+use base64::{Engine as _, engine::general_purpose};
+use futures_util::stream::Stream;
+use futures_util::StreamExt;
+use opencv::core::Vector;
 use opencv::prelude::*;
 use opencv::videoio;
-use std::sync::{Mutex, Arc};
-use opencv::core::Vector;
 use serde::{Deserialize, Serialize};
-use base64::decode;
 
 #[derive(Serialize, Deserialize)]
 struct ControlMessage {
@@ -16,66 +16,63 @@ struct ControlMessage {
 }
 
 struct AppState {
-    camera: Arc<Mutex<Option<videoio::VideoCapture>>>,
+    eyes: Arc<Mutex<Option<videoio::VideoCapture>>>,
 }
 
-struct WebSocketSession {
-    state: web::Data<AppState>,
-}
+fn eyes_stream(state: web::Data<AppState>) -> impl Stream<Item=Result<web::Bytes, Error>> {
+    let mut interval = tokio::time::interval(std::time::Duration::from_millis(100));
+    let state = state.clone();
 
-impl Actor for WebSocketSession {
-    type Context = ws::WebsocketContext<Self>;
-
-    fn started(&mut self, ctx: &mut Self::Context) {
-        let state = self.state.clone();
-        ctx.run_interval(std::time::Duration::from_millis(100), move |act, ctx| {
-            let mut camera_guard = state.camera.lock().unwrap();
-            if let Some(ref mut camera) = *camera_guard {
+    async_stream::stream! {
+        loop {
+            interval.tick().await;
+            let mut eyes_guard = state.eyes.lock().unwrap();
+            if let Some(ref mut eyes) = *eyes_guard {
                 let mut frame = Mat::default();
-                if camera.read(&mut frame).is_ok() {
+                if eyes.read(&mut frame).is_ok() {
                     let mut buf = Vector::new();
                     if opencv::imgcodecs::imencode(".jpg", &frame, &mut buf, &Vector::new()).is_ok() {
-                        ctx.binary(buf.to_vec());
+                        yield Ok(web::Bytes::from(format!("data: {}\n\n", general_purpose::STANDARD.encode(&buf))));
                     }
                 }
             }
-        });
-    }
-}
-
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocketSession {
-    fn handle(&mut self, _: Result<ws::Message, ws::ProtocolError>, _: &mut Self::Context) {}
-}
-
-async fn ws_index(r: HttpRequest, stream: Payload, data: web::Data<AppState>) -> Result<HttpResponse, Error> {
-    // if let Err(e) = authenticate(&r) {
-    //     return Ok(e);
-    // }
-    ws::start(WebSocketSession { state: data }, &r, stream)
-}
-
-async fn turn_camera_on(r: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
-    if let Err(e) = authenticate(&r) {
-        return e;
-    }
-    let mut camera_guard = data.camera.lock().unwrap();
-    if camera_guard.is_none() {
-        *camera_guard = Some(videoio::VideoCapture::new(0, videoio::CAP_V4L2).unwrap());
-        if !camera_guard.as_ref().unwrap().is_opened().unwrap() {
-            *camera_guard = None;
-            return HttpResponse::InternalServerError().body("Unable to open camera");
         }
     }
-    HttpResponse::Ok().body("Camera turned on")
 }
 
-async fn turn_camera_off(r: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
+async fn sensors_eyes_event_stream(data: web::Data<AppState>) -> Result<HttpResponse, Error> {
+    println!("Streaming eyes data...");
+
+    let stream = eyes_stream(data);
+
+    Ok(HttpResponse::Ok()
+        .append_header(("Access-Control-Allow-Credentials", "true"))
+        .content_type("text/event-stream")
+        .streaming(stream))
+}
+
+async fn turn_eyes_on(r: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
     if let Err(e) = authenticate(&r) {
         return e;
     }
-    let mut camera_guard = data.camera.lock().unwrap();
-    *camera_guard = None;
-    HttpResponse::Ok().body("Camera turned off")
+    let mut eyes_guard = data.eyes.lock().unwrap();
+    if eyes_guard.is_none() {
+        *eyes_guard = Some(videoio::VideoCapture::new(2, videoio::CAP_ANY).unwrap());
+        if !eyes_guard.as_ref().unwrap().is_opened().unwrap() {
+            *eyes_guard = None;
+            return HttpResponse::InternalServerError().body("Unable to open eyes");
+        }
+    }
+    HttpResponse::Ok().body("eyes turned on")
+}
+
+async fn turn_eyes_off(r: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
+    if let Err(e) = authenticate(&r) {
+        return e;
+    }
+    let mut eyes_guard = data.eyes.lock().unwrap();
+    *eyes_guard = None;
+    HttpResponse::Ok().body("eyes turned off")
 }
 
 fn authenticate(req: &HttpRequest) -> Result<(), HttpResponse> {
@@ -83,7 +80,7 @@ fn authenticate(req: &HttpRequest) -> Result<(), HttpResponse> {
         if let Ok(auth_str) = auth_header.to_str() {
             if auth_str.starts_with("Basic ") {
                 let encoded = &auth_str[6..];
-                if let Ok(decoded) = decode(encoded) {
+                if let Ok(decoded) = general_purpose::STANDARD.decode(&encoded) {
                     if let Ok(decoded_str) = String::from_utf8(decoded) {
                         let parts: Vec<&str> = decoded_str.split(':').collect();
                         if parts.len() == 2 {
@@ -104,8 +101,8 @@ fn authenticate(req: &HttpRequest) -> Result<(), HttpResponse> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let camera = web::Data::new(AppState {
-        camera: Arc::new(Mutex::new(None)),
+    let eyes = web::Data::new(AppState {
+        eyes: Arc::new(Mutex::new(None)),
     });
 
     let server_addr = "127.0.0.1";
@@ -119,11 +116,11 @@ async fn main() -> std::io::Result<()> {
             .max_age(3600);
 
         App::new()
-            .app_data(camera.clone())
+            .app_data(eyes.clone())
             .wrap(cors)
-            .route("/ws/", web::get().to(ws_index))
-            .route("/camera/on", web::post().to(turn_camera_on))
-            .route("/camera/off", web::post().to(turn_camera_off))
+            .route("/sensors/eyes/event", web::get().to(sensors_eyes_event_stream))
+            .route("/sensors/eyes/on", web::post().to(turn_eyes_on))
+            .route("/sensors/eyes/off", web::post().to(turn_eyes_off))
     })
         .bind((server_addr, server_port))?
         .run();
